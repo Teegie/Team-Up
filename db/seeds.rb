01f2d5e09ca25cc34c29aa7b71c43f05fa372d7d
Teegie <aoubid_h@etna-alternance.net>
2014-12-22 17:27:30 +0100

    Revert "ajout de la balise yield"
    This reverts commit 64c98ab871727a057a650f6edead7421664db7ad.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 #
 # Examples:
 #
-#   clients = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
