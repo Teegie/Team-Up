20fdbfd657ab488e11c72c66b7b011fca3edae15
Metalhearf <dasilv_a@etna-alternance.net>
2015-01-10 05:03:39 +0100

    standard intentation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
-      layout "application.html.erb"
-  def index
-  end
-  def show
-  	render :'home/show.html.erb'
-  end
+	layout "application.html.erb"
+	def index
+	end
+	def show
+		render :'home/show.html.erb'
+	end
 end
